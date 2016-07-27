7cc061cbf5d3bde949d09b0a9ecf84de1c1d6031
xramoli <xaviramos3@gmail.com>
2016-07-27 22:46:40 +0200

    Decoder afegit utilitzant llibreria AVFoundation

--- QR-Coder-Decoder/SecondViewController.h
+++ QR-Coder-Decoder/SecondViewController.h
@@ -7,8 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
 
-@interface SecondViewController : UIViewController
+@interface SecondViewController : UIViewController  <AVCaptureMetadataOutputObjectsDelegate>
 
 
 @end
